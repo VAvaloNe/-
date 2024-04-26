200a6375734c443ed78c025658e9432f9ca7f3e1
VAvaloNe <166640338+VAvaloNe@users.noreply.github.com>
2024-04-26 15:22:33 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+my_list= ['огурец', 'помидор', 'лук', 'капуста', 'свекла', 'чеснок']
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'укроп'
+print(my_list)
+my_dict= {'огурец': 40, 'помидор': 190, 'лук': 60, 'перец': 200}
+print(my_dict)
+my_dict['капуста'] = 120
+print(my_dict)
